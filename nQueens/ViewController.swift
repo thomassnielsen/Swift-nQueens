0865bfea58558e0e08c9d5d21a60e7596d35fe3e
thomassnielsen <me@thomassnielsen.com>
2015-03-26 13:09:38 +0100

    Initial Commit

---
+++ nQueens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nQueens
+//
+//  Created by Thomas Sunde Nielsen on 26.03.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
